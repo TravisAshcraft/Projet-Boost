01edd8bebeec9a44e60e0376bef73699094a80ae
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-01-31 21:49:57 -0600

    Added in key bindings to keyboard

---
+++ Project Boost/Assets/Scripts/Move.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 10f;
+    [SerializeField] float mainThrust = 100f;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] AudioClip mainEngine;
+
+    AudioSource audioSource;
+    Rigidbody myrigidbody;
+
+    public Joystick horizontalJoystick;
+    public JoyButton verticalJoyButton;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        verticalJoyButton = FindObjectOfType<JoyButton>();
+        horizontalJoystick = FindObjectOfType<Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            RespondToRotateInput();
+            ApplyThrust();
+        }
+    }
+
+   
+
+    public void RespondToRotateInput()
+    {
+        transform.Rotate(0, 0, horizontalJoystick.Horizontal * rcsThrust);
+    }
+
+    public  void ApplyThrust()
+    {
+        
+
+        if (verticalJoyButton.Pressed)
+        {
+            
+            float shipThrust = mainThrust * Time.deltaTime;
+            myrigidbody.AddRelativeForce(Vector3.up * shipThrust);
+            mainEngineParticles.Play();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+        
+    }
+}
--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -3,43 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 100f;
     [SerializeField] float levelLoadDelay = 2f;
-
-    [SerializeField] AudioClip mainEngine;
-    [SerializeField] AudioClip deathSFX;
     [SerializeField] AudioClip levelCompleteSFX;
 
-    [SerializeField] ParticleSystem mainEngineParticles;
+   
     [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] AudioClip deathSFX;
     [SerializeField] ParticleSystem levelCompleteParticles;
 
     AudioSource audioSource;
-    Rigidbody myrigidbody;
+    
 
     enum State { Alive, Dying, Transcending }
     State state = State.Alive;
 
-  
+
+
     private void Start()
     {
-        myrigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (state == State.Alive)
-        {
-            RespondToRotateInput();
-            RespondToThrust();
-        }
-      
+        
     }
 
     
@@ -47,7 +39,6 @@ public class Rocket : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if(state != State.Alive) { return; }
-
 
         switch (collision.gameObject.tag)
         {
@@ -91,57 +82,10 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(nextSceneIndex);
         
     }
-    
+
     private void LoadFirstLevel()
     {
         SceneManager.LoadScene(0);
-        
-    }
 
-    private void RespondToRotateInput()
-    {
-        myrigidbody.freezeRotation = true;
-
-        
-        float rotationThisFrame = rcsThrust * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            
-            transform.Rotate(Vector3.forward * rotationThisFrame);
-
-        }
-        else if (Input.GetKey(KeyCode.D)) 
-        {
-            
-            transform.Rotate(-Vector3.forward * rotationThisFrame); 
-        }
-        myrigidbody.freezeRotation = false;
-
-    }
-
-     private void RespondToThrust()
-    {
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            ApplyThrust();
-        }
-        else
-        {
-            audioSource.Stop();
-            mainEngineParticles.Stop();
-        }
-    }
-
-    private void ApplyThrust()
-    {
-        float shipThrust = mainThrust * Time.deltaTime;
-        myrigidbody.AddRelativeForce(Vector3.up * shipThrust);
-        if (!audioSource.isPlaying)
-        {
-            audioSource.PlayOneShot(mainEngine);
-        }
-        mainEngineParticles.Play();
     }
 }
